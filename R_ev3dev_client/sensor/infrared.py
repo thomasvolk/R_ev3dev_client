9eddfaaaa0c8650a7936ced9a5d456e8ccd1f773
Thomas Volk (ThinkPad) <thomas@my-volk.de>
2020-03-20 23:20:00 +0100

    add infrared

---
+++ R_ev3dev_client/sensor/infrared.py
@@ -0,0 +1,13 @@
+import uuid
+from R_ev3dev_client.client import OK
+
+
+class InfraredSensor(object):
+    def __init__(self, client, input_id, ref=str(uuid.uuid4())):
+        self.__client = client
+        self.__ref = ref
+        r = self.__client.send('infrared {} on {}'.format(self.__ref, input_id))
+        assert r == OK
+
+    def distance(self, channel=1):
+        return int(self.__client.send('infrared {} distance {}'.format(self.__ref, channel)))
---
+++ tests/test_infrared.py
@@ -0,0 +1,16 @@
+import unittest
+from R_ev3dev_client.client import Client
+from R_ev3dev_client.sensor.infrared import InfraredSensor
+from R_ev3dev_client.sensor import INPUT_1
+from socket_mock import MockServerSocketModule
+
+
+class TestInfrared(unittest.TestCase):
+    def test_get_color(self):
+        sf = MockServerSocketModule()
+        sf.add_response('infrared 1 on #1', 'ok')
+        sf.add_response('infrared 1 distance 1', 'value int 56')
+        client = Client('test_client', 99999, socket_lib=sf)
+        with client:
+            ir = InfraredSensor(client, INPUT_1, ref='1')
+            self.assertEqual(ir.distance(), 56)
